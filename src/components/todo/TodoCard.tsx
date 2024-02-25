5e5624febacf46c3de93af97330aeae4ff76a0ed
Your Name <rimonron.ad@gmail.com>
2024-02-25 15:03:35 +0600

    Todo card refactor

--- src/components/todo/TodoCard.tsx
+++ src/components/todo/TodoCard.tsx
@@ -21,17 +21,24 @@ const TodoCard = ({title, description, id, isCompleted, priority }: TTodoCardPro
 
     return (
         <div className="bg-white rounded-md flex justify-between items-center p-3 border">
-            <input onChange={toggleState} type="checkbox" name="complete" id="complete" />
-            <p className="font-semibold">{title}</p>
+            <input onChange={toggleState} className="mr-3" type="checkbox" name="complete" id="complete" />
+            <p className="font-semibold flex-1">{title}</p>
             {/* <p>Time</p> */}
-            <div>
+            <div className="flex-1 flex items-center gap-2">
+                <div className={`size-3 rounded-full
+                ${priority === 'high' ? "bg-red-500" : null}
+                ${priority === 'medium' ? "bg-yellow-500" : null}
+                ${priority === 'low' ? "bg-green-500" : null}
+                `}></div>
+                <p className="">{priority}</p>
+            </div>
+            <div className="flex-1">
                 {isCompleted ? 
                 <p className="text-green-500">Done</p> : 
                 <p className="text-red-500">Pending</p>
                 }
             </div>
             <p>{description}</p>
-            <p>{priority}</p>
             <div className="space-x-5">
                 <Button onClick={() => dispatch(removeTodo(id))} className="bg-red-500"><Trash2 /></Button>
                 <Button className="bg-[#5C53FE]"><SquarePen /></Button>
